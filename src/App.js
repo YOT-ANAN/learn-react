4bd004d9f59f67d26d6471cc5322e6bdfc9dc3af
5910110270_Yot-ANAN <32262282+YOT-ANAN@users.noreply.github.com>
2020-05-25 20:54:14 +0700

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -18,52 +18,57 @@ class App extends Component {
     this.state = { products: "" };
   }
   componentDidMount() {
-    this.setState({
-      products: [
-        {
-          productId: 1,
-          productName: "สลัดผัก",
-          unitPrice: "120",
-          thumbnail: "/images/product/1.jpg",
-        },
-        {
-          productId: 2,
-          productName: "ไก่ทอด",
-          unitPrice: "90",
-          thumbnail: "/images/product/2.jpg",
-        },
-        {
-          productId: 3,
-          productName: "บิงซู",
-          unitPrice: "200",
-          thumbnail: "/images/product/3.jpg",
-        },
-        {
-          productId: 4,
-          productName: "เฟรนฟราย",
-          unitPrice: "140",
-          thumbnail: "/images/product/4.jpg",
-        },
-        {
-          productId: 5,
-          productName: "เค้ก 3 ชั้น",
-          unitPrice: "200",
-          thumbnail: "/images/product/5.jpg",
-        },
-        {
-          productId: 6,
-          productName: "กาแฟ เฮลตี้ฟู้ด",
-          unitPrice: "140",
-          thumbnail: "/images/product/6.jpg",
-        },
-      ],
-    });
+    // 1st solution
+    // this.setState({
+    //   products: [
+    //     {
+    //       productId: 1,
+    //       productName: "สลัดผัก",
+    //       unitPrice: "120",
+    //       thumbnail: "/images/product/1.jpg",
+    //     },
+    //     {
+    //       productId: 2,
+    //       productName: "ไก่ทอด",
+    //       unitPrice: "90",
+    //       thumbnail: "/images/product/2.jpg",
+    //     },
+    //     {
+    //       productId: 3,
+    //       productName: "บิงซู",
+    //       unitPrice: "200",
+    //       thumbnail: "/images/product/3.jpg",
+    //     },
+    //     {
+    //       productId: 4,
+    //       productName: "เฟรนฟราย",
+    //       unitPrice: "140",
+    //       thumbnail: "/images/product/4.jpg",
+    //     },
+    //     {
+    //       productId: 5,
+    //       productName: "เค้ก 3 ชั้น",
+    //       unitPrice: "200",
+    //       thumbnail: "/images/product/5.jpg",
+    //     },
+    //     {
+    //       productId: 6,
+    //       productName: "กาแฟ เฮลตี้ฟู้ด",
+    //       unitPrice: "140",
+    //       thumbnail: "/images/product/6.jpg",
+    //     },
+    //   ],
+    // });
+
+    fetch("http://localhost:3001/products", { method: "GET" })
+    .then(res => res.json())
+    .then(res => {this.setState({products:res})})
   }
   render() {
     return (
       <div className="App">
         <Header />
-        <Monitor products={this.state.products}/>
+        <Monitor products={this.state.products} />
         <Footer company="PSU" email="5910110270@psu.ac.th" />
       </div>
     );
