36fb24b499fc5981c06f7e6e836b63356c9853fd
5910110270_Yot-ANAN <32262282+YOT-ANAN@users.noreply.github.com>
2020-05-29 00:10:48 +0700

    render Form Edit

--- src/components/product/ProductForm.js
+++ src/components/product/ProductForm.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
-import {reduxForm} from "redux-form"
+import { reduxForm, Field } from "redux-form"
 class ProductForm extends Component {
+    renderField() {
+        const formFields = [
+            { name: "ProductName", type: "text" },
+            { name: "unitPrice", type: "number" },
+            { name: "thumbnail", type: "text" }
 
-    render(){
-        return(
+        ];
+        return formFields.map(({ name, type }) => {
+            return (
+                <Field name={name} type={type} component="input" />
+            );
+        })
+    }
+
+    render() {
+        return (
             <div>
                 <form>
-
+                    {this.renderField()}
                 </form>
             </div>
         );
     }
 }
 
-ProductForm = reduxForm ({form:"productForm"})(ProductForm)
+ProductForm = reduxForm({ form: "productForm" })(ProductForm)
 
 export default ProductForm;
