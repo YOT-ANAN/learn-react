4fc30231dd116c3af9931f65069e097aaf17b541
5910110270_Yot-ANAN <32262282+YOT-ANAN@users.noreply.github.com>
2020-05-28 23:56:27 +0700

    create ProductForm to edit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import {reducer as reduxForm } from "redux-form"
 import ProductReducer from "./ProductRuducer";
 import OrderReducer from "./OrderReducer";
 
 const rootReducer = combineReducers({
     orders: OrderReducer,
-    products: ProductReducer
+    products: ProductReducer,
+    from : reduxForm
 })
 
 export default rootReducer;
