47b0423a1bc445c05bf70758b0eecac0d6715946
5910110270_Yot-ANAN <32262282+YOT-ANAN@users.noreply.github.com>
2020-05-29 13:59:49 +0700

    Revert "render Form Edit"
    This reverts commit 36fb24b499fc5981c06f7e6e836b63356c9853fd.

--- src/components/product/ProductForm.js
+++ src/components/product/ProductForm.js
@@ -1,31 +1,18 @@
 import React, { Component } from "react";
-import { reduxForm, Field } from "redux-form"
+import {reduxForm} from "redux-form"
 class ProductForm extends Component {
-    renderField() {
-        const formFields = [
-            { name: "ProductName", type: "text" },
-            { name: "unitPrice", type: "number" },
-            { name: "thumbnail", type: "text" }
 
-        ];
-        return formFields.map(({ name, type }) => {
-            return (
-                <Field name={name} type={type} component="input" />
-            );
-        })
-    }
-
-    render() {
-        return (
+    render(){
+        return(
             <div>
                 <form>
-                    {this.renderField()}
+
                 </form>
             </div>
         );
     }
 }
 
-ProductForm = reduxForm({ form: "productForm" })(ProductForm)
+ProductForm = reduxForm ({form:"productForm"})(ProductForm)
 
 export default ProductForm;
